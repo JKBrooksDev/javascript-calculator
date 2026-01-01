87857cbeee4169e5df80c35a6c8e1fe63da20070
Jason Brooks <jasonbrooks1127@gmail.com>
2026-01-01 12:07:06 -0500

    Initial commit - JavaScript Calculator

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer= (props) => {
+  return (
+    <footer>
+      <a href="#" className="author">
+        {" "}
+        Made by <br />
+        <p>Jason Brooks 'AKA: JKB'</p>
+      </a>
+    </footer>
+  );
+}
+
+
+// const Footer = (props) => {
+//   return (
+//     <footer>
+//       <a href="#" target="_blank">
+//         By JKB
+//       </a>
+//     </footer>
+//   );
+// };
+
+
+
+export default Footer
